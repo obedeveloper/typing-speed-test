8db3c5898baa61becf97029c4e5c060c9c834a7d
NIYOMUGISHA Obed <obed6@duck.com>
2026-02-01 17:56:24 +0200

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon-32x32.png';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Frontend Mentor | Typing Speed Test</title>
+</svelte:head>
+
+{@render children()}
