7b3896accbceef533644f5d32b65a5fb9150656b
NIYOMUGISHA Obed <obed6@duck.com>
2026-03-16 19:40:39 +0200

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import './layout.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href="favicon-32x32.png" />
+</svelte:head>
+
+{@render children()}
+
+<div class="attribution">
+	Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. Coded by
+	<a href="https://github.com/obedeveloper">NIYOMUGISHA Obed</a>.
+</div>
